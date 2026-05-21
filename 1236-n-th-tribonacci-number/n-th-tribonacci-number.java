8871e81ff2ec95a008e32d50d1c71abe597f480c
Shashank_03 <shashankgangade77@gmail.com>
2026-05-21 17:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (51.48%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,22 +1,24 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n==0){
-            return 0;
-        }
-        else if (n==1 || n==2){
-            return 1;
-        }
+      if(n==0)
+      {
+        return 0;
+      }
+      else if (n==1 || n==2)
+      {
+        return 1;
+      }
+      int ft = 0;
+      int st = 1;
+      int tt = 1;
+      for (int i = 1;i<=n ; i++)
+      {
+        int fot = ft + st + tt;
+        ft = st;
+        st = tt;
+        tt = fot;
 
-        int ft = 0 ;
-        int st = 1;
-        int tt = 1;
-
-        for(int i = 1 ; i<= n ; i++){
-            int fot = ft + st + tt;
-            ft = st;
-            st = tt;
-            tt = fot;
-        }
-        return ft;
+      }
+      return ft;
     }
 }
