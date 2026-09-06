7e6c4f15d5cb5f8073d892505a897e6e43c067fa
Shashank_03 <shashankgangade77@gmail.com>
2026-09-06 18:38:20 +0530

    Time: 17 ms (80.61%) | Memory: 49.7 MB (35.44%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int max = 0;
         for(int i = 0 ; i<nums.length ; i+=2){
-            
                 max += nums[i];
-        
         }
         return max;
     }
